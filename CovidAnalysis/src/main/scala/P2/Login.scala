7cafbda40722d47a037e51fc19b956640bf1e36d
Oscar Garcia <ogarcia2834@outlook.com>
2022-07-15 14:38:38 -0500

    DELTA6-11 validateLogin function created

--- CovidAnalysis/src/main/scala/P2/Login.scala
+++ CovidAnalysis/src/main/scala/P2/Login.scala
@@ -11,7 +11,7 @@ object Login {
   private var df: DataFrame = _
 
   def connect(): Unit = {
-    System.setProperty("hadoop.home.dir", "C:\\hadoop")
+    System.setProperty("hadoop.home.dir", "C:\\hadoop3")
     spark = SparkSession
       .builder()
       .appName("Spark api")
@@ -22,14 +22,22 @@ object Login {
 
     spark.sparkContext.setLogLevel("ERROR")
     spark.sql("Set hive.exec.dynamic.partition.mode=nonstrict")
+
+
+
+    spark.sql("DROP table IF EXISTS UserInfo")
+    spark.sql("create table IF NOT EXISTS UserInfo(FirstName String, LastName String, Username String,Password String,AdminPriv Int) row format delimited fields terminated by ','")
+    //spark.sql("LOAD DATA LOCAL INPATH 'userinfo.csv' INTO TABLE UserInfo")
+    spark.sql("SELECT * FROM UserInfo").show()
+    spark.sql("SELECT * FROM UserInfo").printSchema()
   }
 
 
   def createUser(FirstName: String, LastName: String, Username: String, Password: String, AdminPriv: Int): Unit = {
 
     try {
-    spark.sql(f"INSERT INTO UserInfo(FirstName,LastName,Username, Password, AdminPriv) VALUES('$FirstName', '$LastName','$Username','$Password','$AdminPriv')")
-    //spark.sql("SELECT * FROM UserInfo").show()
+    spark.sql(f"INSERT INTO UserInfo(FirstName,LastName,Username, Password, AdminPriv) VALUES('$FirstName', '$LastName','$Username','$Password',$AdminPriv)")
+    spark.sql("SELECT * FROM UserInfo").show()
     }
     catch{
       case e: SQLException => e.printStackTrace()
@@ -79,15 +87,56 @@ object Login {
 
     def updateName(newFirstName: String, newLastName: String,existingUser : String): Unit = {
       try {
-        spark.sql(f"UPDATE users SET Firstname = '$newFirstName', Lastname = '$newLastName'  WHERE Username = '$existingUser'")
+        spark.sql(f"UPDATE UserInfo SET Firstname = '$newFirstName', Lastname = '$newLastName'  WHERE Username = '$existingUser'")
       }
       catch {
         case e: Exception => e.printStackTrace()
       }
-
-
     }
 
+  def elevate2Admin(selectedUser: String): Unit = {
+    try {
+      spark.sql(f"UPDATE UserInfo SET AdminPriv = 1 where username = '$selectedUser'")
+    }
+    catch {
+      case e: Exception => e.printStackTrace()
+    }
+  }
+
+
+  def deleteUser(delUser: String): Unit = {
+    try {
+      spark.sql(f"Delete From UserInfo where username = '$delUser'")
+    }
+    catch {
+      case e: Exception => e.printStackTrace()
+    }
+  }
+
+  def validateLogin(usersUserName: String, usersPassword: String): Boolean = {
+
+    try {
+      df = spark.sql(f"SELECT * From UserInfo WHERE Username = '$usersUserName' AND password = '$usersPassword'")
+    }
+    catch {
+      case e: Exception => e.printStackTrace()
+    }
+
+      if (df.isEmpty) {
+        println("Username and Password are incorrect")
+        print("")
+        false
+
+      } else {
+        println("Username and Password are correct.")
+        print("")
+        true
+      }
+
+
+
+
+  }
 
 
 
