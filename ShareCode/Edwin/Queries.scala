9f449a8006c9a7d1c1c40db3893c532a5fd33cf6
JEDWINCASTA <escore99@gmail.com>
2022-07-22 01:46:00 -0400

    saving data in parquet format, creating tables and making queries

--- ShareCode/Edwin/Queries.scala
+++ ShareCode/Edwin/Queries.scala
@@ -10,7 +10,7 @@ import java.util.Scanner
 
 object Queries{
   def main(args: Array[String]): Unit = {
-    System.setProperty("hadoop.home.dir", "C:\\Hadoop3")
+    System.setProperty("hadoop.home.dir", "C:\\hadoop")
     val scanner = new Scanner(System.in)
     val spark = SparkSession
       .builder
@@ -23,32 +23,48 @@ object Queries{
 
     println("created spark session")
 
+    createTables()
+
     def createTables(): Unit = {
 
-
-      val dfcovid = spark.read.option("header", true).csv("hdfs://localhost:9000/user/hive/warehouse/covid_19_data.csv")
-      dfcovid.write.mode(SaveMode.Overwrite).csv("hdfs://localhost:9000/user/hive/warehouse/new_covid_19_data")
-
-      val sc=spark.sparkContext
-      val rddCovidFromFile = sc.textFile("/user/hive/warehouse/new_covid_19_data")
-      val rddCovid = rddCovidFromFile.map(f=>{f.split(",")})
-      rddCovid.toDS.show(false)
-
       import spark.implicits._
-      case class City(CityID: Long, Province_State: String)
-      val dfcit=rddCovid.map(attributes => City(attributes(0).trim.toLong, attributes(2))).toDF()
-      dfcit.createOrReplaceTempView("cities")
-      spark.sql("SELECT * FROM cities").show()
 
-      case class Country(CountryID: Long, Country: String)
-      val dfcount=rddCovid.map(attributes => Country(attributes(0).trim.toLong, attributes(3))).toDF()
-      dfcount.createOrReplaceTempView("countries")
-      spark.sql("SELECT * FROM countries").show()
+//      val dfcovid = spark.read.option("header", true).csv("hdfs://localhost:9000/user/hive/warehouse/covid_19_data.csv")
+//      dfcovid.write.mode(SaveMode.Overwrite).csv("hdfs://localhost:9000/user/hive/warehouse/new_covid_19_data")
+      val dfcovidFromFile = spark.read.option("delimiter", ",").option("inferSchema","true").option("header", "true")
+                .csv(s"hdfs://localhost:9000/user/hive/warehouse/covid_19_data.csv")
 
-      case class Covid_data(SNo: Long, ObservationDate: String, Last_Update: String, Confirmed: String, Deaths: String, Recovered: String)
-      val dfcovid=rddCovid.map(attributes => Covid_data(attributes(0).trim.toLong, attributes(1), attributes(4), attributes(5), attributes(6), attributes(7))).toDF()
-      dfcovid.createOrReplaceTempView("covid")
-      spark.sql("SELECT * FROM covid").show()
+      dfcovidFromFile.createOrReplaceTempView("cd")
+
+      val dfCovid_data=(dfcovidFromFile.withColumnRenamed("Country/Region", "region_country")
+        .withColumnRenamed("Province/State", "province_state")
+        .withColumnRenamed("Last Update", "Last_Update")
+        .withColumn("Confirmed", col("Confirmed").cast("int"))
+        .withColumn("Deaths", col("Deaths").cast("int"))
+        .withColumn("Recovered", col("Recovered").cast("int"))
+        )
+
+      dfCovid_data.show()
+      dfCovid_data.createOrReplaceTempView("covid_view")
+      dfCovid_data.repartition(10).write.mode("overwrite").save("hdfs://localhost:9000/user/hive/warehouse/new_covid_19_data.parquet")
+      val parquetfiledf = spark.read.parquet("hdfs://localhost:9000/user/hive/warehouse/new_covid_19_data.parquet")
+      parquetfiledf.createOrReplaceTempView("parquet_view")
+      spark.sql("SELECT * FROM parquet_view order by SNo ASC").show(25)
+
+      /*Table for Cities*/
+      spark.sql("CREATE TABLE IF NOT EXISTS cities AS SELECT SNo, province_state FROM parquet_view")
+      spark.sql("select * from cities order by SNo ASC").show()
+
+
+       /*Table for Countries*/
+      spark.sql("CREATE TABLE IF NOT EXISTS countries AS SELECT SNo, region_country FROM parquet_view")
+      spark.sql("select * from countries order by SNo ASC").show()
+
+
+        /*Table for covid_data*/
+      spark.sql("CREATE TABLE IF NOT EXISTS covid_data AS SELECT SNo, ObservationDate, Last_Update, " +
+        "Confirmed, Deaths, Recovered FROM parquet_view")
+      spark.sql("select * from covid_data order by SNo ASC").show()
 
     }
 
@@ -56,18 +72,23 @@ object Queries{
 
       Enter your query here
  */
+    spark.sql("SELECT ObservationDate, COUNT(confirmed) AS Total_confirmed FROM covid_data where ObservationDate BETWEEN '04/01/2020' AND '06/30/2020' " +
+      "GROUP BY ObservationDate ORDER BY ObservationDate ASC").show()
 
 /*    - How many people were recovered worldwide by the last quarter of 2020
 
       Enter your query here
  */
+    spark.sql("SELECT ObservationDate, COUNT(recovered) AS Total_Recovered FROM covid_data where ObservationDate BETWEEN '10/01/2020' AND '12/31/2020' " +
+      "GROUP BY ObservationDate ORDER BY ObservationDate ASC").show()
 
 
 /*    - What are the top 10 cities with number of deaths in the US?
 
       Enter your query here
  */
-
+    spark.sql("SELECT province_state, COUNT(deaths) AS Total_Deaths FROM cities c JOIN covid_data cov ON (c.sno = cov.sno) JOIN countries co " +
+      "ON (c.sno = co.sno) WHERE co.region_country = 'US' GROUP BY province_state ORDER BY Total_Deaths DESC LIMIT 10").show()
 
 /*    - What are the top 10 cities with least COVID cases in the US?
 
