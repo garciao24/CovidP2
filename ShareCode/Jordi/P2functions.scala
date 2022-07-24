51fea8ce7db05eda3ee3bdb911c00e831dbad55a
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-07-24 18:42:25 -0400

    Final Functions Done

--- ShareCode/Jordi/P2functions.scala
+++ ShareCode/Jordi/P2functions.scala
@@ -25,7 +25,7 @@ object P2functions {
   def totalbystates()={
     println("Total confirmed by state.")
     spark.sql("SELECT Province_State, SUM(`5/1/21`) AS DatedTotal " +
-      "FROM CovConUS GROUP BY Province_State ORDER BY DatedTotal DESC").show(100,false)
+      "FROM CovConUS GROUP BY Province_State ORDER BY DatedTotal DESC").show(10000,false)
   }
   //----------------------------------------------------------------------------------------------------- Input two dates get new cases.
   def datedchange()={
@@ -34,14 +34,14 @@ object P2functions {
     println(f"New cases based on new cases from $initialdate to $finaldate")
 
     spark.sql(f"SELECT Province_State, SUM($finaldate)-SUM($initialdate) AS NewCases " +
-      "FROM CovConUS GROUP BY Province_State ORDER BY NewCases DESC").show(100,false)
+      "FROM CovConUS GROUP BY Province_State ORDER BY NewCases DESC").show(10000,false)
   }
   //----------------------------------------------------------------------------------------------------- Total confirmed in a day.
   def totalday()= {
     var stringeddate = "`5/2/21`" //scala.io.StdIn.readLine()
     println(f"Total for $stringeddate")
     spark.sql(f"SELECT Province_State, SUM($stringeddate) AS Ratio " +
-      "FROM CovConUS GROUP BY Province_State ORDER BY Ratio DESC").show(100,false)
+      "FROM CovConUS GROUP BY Province_State ORDER BY Ratio DESC").show(10000,false)
   }
   //----------------------------------------------------------------------------------------------------- Total new confirmed in a month.
   def monthrange() = {
@@ -51,7 +51,7 @@ object P2functions {
     var yearimp = f"$yr`"
     var finalimpstring = ""
     var x = 0
-    var firstiimp = ""
+    var firstimp = ""
 
     // Save table into variable, gets header, makes string, then makes list.
     var chosendf = "CovConUS" //scala.io.StdIn.readLine()
@@ -67,13 +67,13 @@ object P2functions {
       if (x == 0 && i.contains("`4/") && i.contains("21`")){  // Adds first value only.
         //println(f"$i Added at start.")
         finalimpstring += f"$i "
-        firstiimp = i
+        firstimp = i
         x = 1
       }
 
       else if (x == 1 && i.contains("`4/") && i.contains("/21`")){ // Adds value at the end.
         //println(f"$i Added at end.")
-        finalimpstring = f"$i - $firstiimp"
+        finalimpstring = f"$i - $firstimp"
       }
 
       else{
@@ -82,27 +82,49 @@ object P2functions {
 
     })
     spark.sql(f"SELECT Province_State, SUM($finalimpstring) AS April_Total " +
-      "FROM CovConUS GROUP BY Province_State ORDER BY April_Total DESC").show(100,false)
+      "FROM CovConUS GROUP BY Province_State ORDER BY April_Total DESC").show(10000,false)
 
     //println(finalimpstring)
     //println(listofdates.length)
 
   }
-/*
+
   def daily(): Unit ={ // Subtract current day minus previous day, for first day just print that day. Show all deaths worldwide by country.
-    listofdates.foreach( i+1 =>
+    var chosendf = "CovDeaths" //scala.io.StdIn.readLine()
+    var currdf = spark.sql(f"SELECT * FROM $chosendf")
+    var ColumnNames=currdf.columns
+    var Columnstring = ColumnNames.mkString("`", "`,`", "`")
+    var Columnlist = Columnstring.split(",")
 
-    spark.sql(f"SELECT Province_State, SUM($finalimpstring) AS April_Total " +
-      "FROM CovConUS GROUP BY Province_State ORDER BY April_Total DESC").show(100,false)
+    var finalimpstring = "SUM(`1/22/20`)"
+    var x = 0
+    //println(Columnlist.mkString(","))
 
-  }*/
+    var firstimp = Columnlist.indexOf("`1/22/20`")
+    var lastimp = Columnlist.length
+    //println(firstimp)
+    //println(lastimp)
 
-  //connectlink()
-  //totalbystates()
+    for ( i <- firstimp + 1 until lastimp){  //Until excludes last number, to includes it.
+      var d1= Columnlist(i)
+      var d2 = Columnlist(i-1)
+      finalimpstring += f", Sum($d1 - $d2)"
+      //(finalimpstring)
+
+    }
+    println("Daily Deaths From Covid Worldwide")
+    spark.sql(f"SELECT `Country/Region`, $finalimpstring " +
+      "FROM CovDeaths GROUP BY `Country/Region` ORDER BY `Country/Region` DESC").show(10000,false)
+
+  }
+
+  //connectlink()park
   //datedchange()
   //totalday()
-  monthrange()
-  //dayrange()
+
+  totalbystates() //<------------------ This one for Project 2
+  monthrange() //<------------------ This one for Project 2
+  daily() //<------------------ This one for Project 2
 
   spark.sql("SHOW DATABASES").show()
   spark.sql("SHOW TABLES").show()
